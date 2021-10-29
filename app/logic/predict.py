14c3e17fa9deba61121afb6c51672d92e42e833b
Geovane Barros <geovanebarros@pop-os.localdomain>
2021-10-29 01:31:24 -0300

    Finalização atividade 5 e 6

--- app/logic/predict.py
+++ app/logic/predict.py
@@ -1,3 +1,29 @@
-def perform_prediction(sample):
-    predicted_class = 'predicted_class'
-    return predicted_class
+import pandas as pd
+import numpy as np
+import joblib
+
+def pre_processing(passenger):
+    df = {}
+    df['Pclass'] = int(passenger.pClass)
+    if passenger.sex == 'male':
+        df['Sex'] = 1
+    else:
+        df['Sex'] = 0
+    df['Age'] = passenger.age
+    df['SibSp'] = passenger.sibSp
+    df['Parch'] = passenger.parch
+
+    return df 
+
+
+def perform_prediction(passenger):
+    df = pre_processing(passenger)
+    X_test = pd.DataFrame(df, index=[0])
+    model_load = joblib.load('./logic/classifier/titanic.pkl')
+    predict = model_load.predict(X_test)
+    if predict == 1:
+        return 'Survived'
+    else:
+        return 'Not survived'
+    
+
